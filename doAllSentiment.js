9b86f4fd3312d1459aa977f402fe50ff62bbdd5d
Clemens Brunner <cle1000.cb@gmail.com>
2017-12-11 08:48:05 +0100

    doall

--- doAllSentiment.js
+++ doAllSentiment.js
@@ -10,16 +10,39 @@ var analysis = require('./sentimental/sentimental.js');
 mongoose.Promise = require('bluebird');
 mongoose.connect(config.mongo_server, {useMongoClient: true});
 
-TweetCollection.find({},['tweets'],{}, function(err, tweetCollections) {
-    var size = _.size(tweetCollections);
-    var i = 0;
-    for (i = 0; i < size; i++){
-        log.info(`${i}/${size}`);
-        if (_.size(tweetCollections[i].tweets) != 0 && _.last(tweetCollections[i].tweets).sentiment != undefined) {
-            tweetCollections[i].tweets = analysis.callSentimentalAnalysis(tweetCollections[i].tweets);
-            tweetCollections[i].save();
-        }
-    }
+
+var close = (msg) => {
+    log.info(msg);
     mongoose.disconnect();
-});
+}
+
+var run = () => {
+    TweetCollection.find(
+        {tweets: {$elemMatch: {sentiment: undefined}}},
+        ['tweets'],
+        {limit: 300},
+        (err, tweetCollections) => {
+            if (err)
+                log.error(err);
+            var size = _.size(tweetCollections);
+            if (size == 0) close("nothing found");
+
+            for (var i = 0; i < size; i++) {
+                log.info(`${i + 1}/${size}`);
+                if (_.size(tweetCollections[i].tweets) != 0 && _.last(tweetCollections[i].tweets).sentiment != undefined) {
+                    TweetCollection.update(
+                        {_id: tweetCollections[i]._id},
+                        {$set: {tweets: analysis.callSentimentalAnalysis(tweetCollections[i].tweets)}},
+                        (err, t) => {
+                            if (err) log.error("Error on save: " + err);
+                            if (size == i+1) close("last element processed");
+                        }
+                    );
+                }
+        }
+    })
+};
+
+
+run();
 
